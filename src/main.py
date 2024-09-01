b99f10aa87c434e91b173cf441631ad769e932ea
Billones142 <stefanomerinoderui@gmail.com>
2024-09-01 00:48:27 -0300

    uso de modulo jsonParse en main

--- src/main.py
+++ src/main.py
@@ -1,16 +1,4 @@
-import json
-from os import path
-from types import SimpleNamespace
+from jsonParse import parsePaginasJson
 
 jsonPath= 'paginasAScrapear.json'
-
-
-def parseJson(jsonPath):
-  #try:
-    with open(path.join(path.dirname(__file__) , jsonPath), 'r') as jsonPaginasAScrappear:
-      paginasAScrapear= json.loads(jsonPaginasAScrappear.read(), object_hook=lambda d: SimpleNamespace(**d))
-      return paginasAScrapear;
-  #except:
-  #  return None
-
-print(parseJson(jsonPath))
+print(parsePaginasJson(jsonPath))
