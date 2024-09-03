27084451f2f6b1272a7af8fedef5cc5cf6f55d9b
Billones142 <stefanomerinoderui@gmail.com>
2024-09-03 10:59:05 -0300

    OuOUoUoUOuououOU Loz teZt no ivAn en maIn??

--- src/mainServer.py
+++ src/mainServer.py
@@ -1,44 +1,13 @@
-from jsonParse import parsePaginasJson
+from utils.jsonParse import parsePaginasJson
 from scrappingActor import ScraperActor, ActorRef
 from typing import Tuple
 import asyncio
-import Pyro5
+import Pyro5.api
 
 jsonPath= 'paginasAScrapear.json'
 
-
 async def main():
-    urlsToScrape= parsePaginasJson(jsonPath)
-
-    # Lista para almacenar las referencias de los actores
-    #actor_refs = list[(ActorRef[ScraperActor],str)]
-    actorRefs: list[Tuple[ActorRef[ScraperActor], str]]= []
-
-    # Crear actores para cada tarea de scraping
-    for datos in urlsToScrape:
-        actor_ref = ScraperActor.start()
-        actorRefs.append((actor_ref, datos.url))
-
-
-    # Ejecutar el scraping y obtener los resultados
-    for actorData, url in actorRefs:
-        htlmString = actorData.ask({'command': 'scrape', 'url': url})
-        print(f"Titles from {url}: {htlmString}")
-
-    # Detener los actores
-    for actor_ref, _ in actorRefs:
-        actor_ref.stop()
-
-def main2():
-    uri = input("Introduce la URI del servidor: ")
-    server = Pyro5.api.Proxy(uri)
-
-    url_to_scrape = "http://mercadolibre.com"
-    print(f"Enviando solicitud de scraping para {url_to_scrape}...")
-    result = server.start_actor(url_to_scrape)
-    
-    print("Resultados obtenidos:")
-    print(result)
+    None
 
 if __name__ == '__main__':
-    asyncio.run(main= main2())
+    asyncio.run(main= main())
--- src/test/test.py
+++ src/test/test.py
@@ -1,11 +1,49 @@
 import unittest
-import mainServer
+from typing import Tuple
+import Pyro5.api
+# importaciones propias del proyecto
+from scrappingActor import ScraperActor, ActorRef
+from utils.jsonParse import parsePaginasJson
+
+jsonPath= 'paginasAScrapear.json'
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_ScrappingActor_scrapeHtml(self):
+        urlsToScrape= parsePaginasJson(jsonPath)
 
+        # Lista para almacenar las referencias de los actores
+        actorRefs: list[ Tuple[ ActorRef[ScraperActor] , str ] ]= []
+
+        # Crear actores para cada tarea de scraping
+        for datos in urlsToScrape:
+            actor_ref = ScraperActor.start()
+            actorRefs.append((actor_ref, datos.url))
+
+
+        # Ejecutar el scraping y obtener los resultados
+        htmlList= []
+        for actorData, url in actorRefs:
+            htlmString = actorData.ask({'command': 'scrape', 'url': url})
+            htmlList.append(htlmString)
+
+        # Detener los actores
+        for actor_ref, _ in actorRefs:
+            actor_ref.stop()
+        self.assertNotEqual(htlmString, None)
+    
+    def test_Pyro5_ScrappingActor():
+        uri = input("Introduce la URI del servidor: ")
+        server = Pyro5.api.Proxy(uri)
+
+        url_to_scrape = "http://mercadolibre.com"
+        print(f"Enviando solicitud de scraping para {url_to_scrape}...")
+        result = server.start_actor(url_to_scrape)
+        
+        print("Resultados obtenidos:")
+        print(result)
+
+    """ ejemplos
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
@@ -16,6 +54,7 @@ class TestStringMethods(unittest.TestCase):
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
+    """
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
