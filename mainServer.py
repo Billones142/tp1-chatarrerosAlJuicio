440e72bbd43cd98f6610ebbacfc90219862b1fda
Billones142 <stefanomerinoderui@gmail.com>
2024-09-11 00:49:38 -0300

    ejemplo de uso de la API

--- mainServer.py
+++ mainServer.py
@@ -10,9 +10,10 @@ from src.websocket import API_ActorsServer
 jsonPath= 'paginasAScrapear.json'
 
 async def main(): # TODO: interfaz
-  async with API_ActorsServer(f"ws://127.0.0.1:{8765}") as serverActores:
-    response= await serverActores.ask_scrapper("https://youtube.com") # elegido por su complejidad y longitud
-    print("respuesta:",response)
+    actorsServer= API_ActorsServer(f"ws://127.0.0.1:{8765}")
+    async with actorsServer:
+        response= await actorsServer.ask_scrapper("https://youtube.com") # elegido por su complejidad y longitud
+        print("respuesta:",response)
 
 if __name__ == '__main__':
-  asyncio.run(main= main())
+    asyncio.run(main= main())
