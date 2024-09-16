b94eccc5a8856a2e48179bff87a8b66cb016bb3a
Billones142 <stefanomerinoderui@gmail.com>
2024-09-16 12:40:22 -0300

    tests utils teminados

--- src/test/utils_test.py
+++ src/test/utils_test.py
@@ -4,9 +4,29 @@ from src import utils
 
 jsonPath= '../paginasAScrapear.json'
 
-class TestCommunication(unittest.IsolatedAsyncioTestCase): # TODO
-    def test(self):
-        utils.parsePaginasJson(jsonPath)
+class TestCommunication(unittest.IsolatedAsyncioTestCase):
+    def test_parsePaginasJson(self):
+        paginasJson= utils.parsePaginasJson(jsonPath)
+        
+        self.assertGreaterEqual(len(paginasJson),2)
+
+        for pagina in paginasJson:
+            self.assertIsNotNone(pagina.nombre)
+            self.assertGreaterEqual(len(pagina.linksDeCompra),1)
+
+    def test_getOrigin(self):
+        url= "https://listado.mercadolibre.com.ar/rtx-2070#D[A:rtx%202070]"
+        parsedUrl= utils.get_origin(url)
+
+        self.assertNotEqual(url, parsedUrl)
+        self.assertEqual("https://listado.mercadolibre.com.ar",parsedUrl)
+    
+    def test_getMainOrigin(self):
+        url= "https://listado.mercadolibre.com.ar/rtx-2070#D[A:rtx%202070]"
+        parsedUrl= utils.get_main_origin(url)
+        
+        self.assertNotEqual(url, parsedUrl)
+        self.assertEqual("https://mercadolibre.com.ar",parsedUrl)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
