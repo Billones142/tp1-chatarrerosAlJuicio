2e83e7f4cc4ef92f395d24de083e596b1d6f1591
Stiven Alexis <stiven22monsalvo@gmail.com>
2024-09-17 12:52:27 -0300

    se añadio en utils encontrarproducto

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,8 +1,13 @@
 from .jsonParse import parseJson, parsePaginasJson
 from .singleton import Singleton
 from .urlOrigin import get_origin, get_main_origin
+from .encontrarElProductoMasBarato import limpiar_precio, encontrar_producto_mas_barato
 
 __all__= ["parseJson",
           "parsePaginasJson",
           "Singleton",
-          "get_origin"]
+          "get_origin",
+          "limpiar_precio",
+          "encontrar_producto_mas_barato",
+          "get_main_origin"
+          ]
