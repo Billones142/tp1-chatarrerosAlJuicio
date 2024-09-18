78e0c490dad77782dc08bec5cd98d67c1af1b898
gmartineza <gabymartinezalarcon@gmail.com>
2024-09-18 12:21:58 -0300

    mainActors se detiene apropiadamente ante un ctrl+c

--- mainActors.py
+++ mainActors.py
@@ -5,20 +5,31 @@ from src import WebSocket_ActorServer
 
 # Configuración específica para cada módulo
 logging.getLogger('asyncio').setLevel(logging.WARNING)
-logging.getLogger('pykka').setLevel(logging.INFO)
+logging.getLogger('pykka').setLevel(logging.WARNING)
 logging.getLogger('websockets').setLevel(logging.INFO)
 
 
-def main():
+async def run_server(event):
     port = 8765
-    event = asyncio.Event()
-    
-    # Iniciar WebSocket_ActorServer
     server = WebSocket_ActorServer(port=port, host="0.0.0.0", stop_flag=event)
+    
     try:
-        asyncio.run(server.serverLoop())
+        await server.serverLoop()
+    except asyncio.CancelledError:
+        pass
+    finally:
+        # Ensure that the server services are stopped
+        await server.stopServerServices()
+    
+def main():
+    event = asyncio.Event()
+    try:
+        asyncio.run(run_server(event))
     except KeyboardInterrupt:
+        print("Ctrl+c recibido, detendiendo servidor...")
         event.set()
+    finally:
+        print("Servidor detenido.")
 
 if __name__ == "__main__":
     main()
